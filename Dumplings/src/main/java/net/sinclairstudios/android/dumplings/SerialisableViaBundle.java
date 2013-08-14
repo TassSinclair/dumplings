3450226a1f9e0d97cd9c5c07e234dc762940bdef
Tass Sinclair <andrew@sinclairstudios.net>
2013-08-14 23:29:12 +1000

    Bugfixes and code cleanups.
    RatingListDataController no longer implemens unused methods.
    HowManyPeopleActivity accurately reports the number of people to the
    YourOrderActivity.

--- Dumplings/src/main/java/net/sinclairstudios/android/dumplings/SerialisableViaBundle.java
+++
@@ -1,8 +0,0 @@
-package net.sinclairstudios.android.dumplings;
-
-import android.os.Bundle;
-
-public interface SerialisableViaBundle {
-    void populate(Bundle bundle);
-    void depopulate(Bundle bundle);
-}
--- Dumplings/src/main/java/net/sinclairstudios/android/dumplings/SerialisableViaResources.java
+++
@@ -1,9 +0,0 @@
-package net.sinclairstudios.android.dumplings;
-
-import android.content.res.Resources;
-import android.os.Bundle;
-
-public interface SerialisableViaResources {
-    void populate(Resources bundle);
-    void depopulate(Resources bundle);
-}
