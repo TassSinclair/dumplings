5a77a99d46944493737ca5356a41ea901500f58f
Andrew Hamilton <andrew@sinclairstudios.net>
2014-03-03 16:25:32 +1100

    Using Kotlin string

--- Dumplings/src/main/kotlin/net/sinclairstudios/android/dumplings/domain/Dumpling.kt
+++ Dumplings/src/main/kotlin/net/sinclairstudios/android/dumplings/domain/Dumpling.kt
@@ -2,7 +2,8 @@ package net.sinclairstudios.android.dumplings.domain
 import java.io.Serializable
 
 public class Dumpling(val name : String) : Serializable {
+    
     public override fun toString() : String {
-        return name + " dumpling"
+        return "$name dumpling"
     }
 }
